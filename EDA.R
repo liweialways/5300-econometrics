bf71000a1068be956837f410805b11a5a3868333
Wei Li <liweialways@gmail.com>
2019-01-17 11:17:13 -0800

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -1,11 +1,21 @@
-# Load data
+### Load data
 wa_data <- read.csv('ss16pwa.csv')
-View(wa_data)
 
-<<<<<<< HEAD
+### Subset and focus on the data of interest
+# SERIALNO: Housing unit/GQ person serial number  # SPORDER: Person number
+# WAGP: Wages or salary income past 12 months     # SCHL: Educational attainment
+# RAC1P/RAC3P: Recoded detailed race code
 research_data <- wa_data[c('SERIALNO','SPORDER','WAGP','SCHL','RAC1P','RAC3P')]
 View(research_data)
-=======
+nrow(research_data)
 
+### EDA
+# exclude all missing values(because we have a large dataset)
+summary(research_data)
+research_data
+new <- na.exclude(research_data) # exclude all missing values
+summary(new)
+nrow(new)
 
->>>>>>> 7dc336027a2de5a0d8c6f7c400762964a0dd296c
+# Distribution of the variables of interest
+dist(new$WAGP)
